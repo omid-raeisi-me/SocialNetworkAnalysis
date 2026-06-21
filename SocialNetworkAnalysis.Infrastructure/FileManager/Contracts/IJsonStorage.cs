cb8f3e0fb6d9ed73735ed187ffe67b593f44d0a8
OMID <OMID@DESKTOP-B08LV52>
2026-06-21 17:02:29 +0330

    Create Infrastructure.FileManager

---
+++ SocialNetworkAnalysis.Infrastructure/FileManager/Contracts/IJsonStorage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetworkAnalysis.Infrastructure.FileManager.Contracts
+{
+    public interface IJsonStorage<T>
+    {
+        Task<T> ReadAsync(CancellationToken cancellationToken);
+        Task WriteAsync(T data, CancellationToken cancellationToken);
+    }
+}
---
+++ SocialNetworkAnalysis.Infrastructure/FileManager/Services/JsonStorage.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using SocialNetworkAnalysis.Infrastructure.FileManager.Contracts;
+
+namespace SocialNetworkAnalysis.Infrastructure.FileManager.Services
+{
+    public class JsonStorage<T> : IJsonStorage<T>
+    {
+        private readonly string _filePath;
+
+        public JsonStorage(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public async Task<T> ReadAsync(CancellationToken cancellationToken)
+        {
+            if (!File.Exists(_filePath))
+                return default;
+
+            await using var stream = new FileStream(
+                _filePath,
+                FileMode.Open,
+                FileAccess.Read,
+                FileShare.Read,
+                bufferSize: 4096,
+                useAsync: true);
+
+            return await JsonSerializer.DeserializeAsync<T>(stream, cancellationToken: cancellationToken);
+        }
+
+        public async Task WriteAsync(T data, CancellationToken cancellationToken)
+        {
+            await using var stream = new FileStream(
+                _filePath,
+                FileMode.Create,
+                FileAccess.Write,
+                FileShare.None,
+                bufferSize: 4096,
+                useAsync: true);
+
+            await JsonSerializer.SerializeAsync<T>(
+                stream,
+                data,
+                new JsonSerializerOptions { WriteIndented = true },
+                cancellationToken);
+        }
+    }
+}
