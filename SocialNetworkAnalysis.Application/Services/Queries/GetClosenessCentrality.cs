16d249b227726ffb58d81d810cf6c7d8afafe861
Amirhossein <amirhossein.shourabi@gmail.com>
2026-08-04 12:31:53 +0330

    Edite Application Layer (add ExecuteSnapshotAsync method)

--- SocialNetworkAnalysis.Application/Services/Queries/GetClosenessCentrality.cs
+++ SocialNetworkAnalysis.Application/Services/Queries/GetClosenessCentrality.cs
@@ -19,7 +19,7 @@ namespace SocialNetworkAnalysis.Application.Services.Queries
 
         public ClosenessCentralityResponse Execute()
         {
-            return _runtime.ExecuteRead(graph =>
+            return _runtime.ExecuteSnapshotAsync(graph =>
             {
                 var response = new ClosenessCentralityResponse();
 
@@ -52,7 +52,7 @@ namespace SocialNetworkAnalysis.Application.Services.Queries
                 response.MaxScore = Math.Round(coreResult.maxScore, 4);
 
                 return response;
-            });
+            }).GetAwaiter().GetResult();
         }
     }
 }
