bd8e11523d23b40e23862f5214d26ebef645bc10
blackduck-fs <alifallah20151385@gmail.com>
2026-08-08 07:30:39 +0330

    Add page-FriendSuggestion

---
+++ SocialNetworkAnalysis.Presentation/Components/Pages/FriendSuggestion.razor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+using SocialNetworkAnalysis.Application.Abstractions.Queries;
+using SocialNetworkAnalysis.Application.DTOs;
+
+namespace SocialNetworkAnalysis.Presentation.Components.Pages
+{
+    public partial class FriendSuggestion
+    {
+        [Parameter]
+        public int Id { get; set; }
+
+        [Inject]
+        private IGetFriendSuggestionsService _getFriendSuggestionsService { get; set; }
+
+        private FriendSuggestionDto _friendSuggestionDto = new();
+        private string _topNumber = "";
+
+        private void Run()
+        {
+            try
+            {
+                if (_topNumber.Trim() == "")
+                    _friendSuggestionDto = _getFriendSuggestionsService.Execute(Id);
+                else
+                    _friendSuggestionDto = _getFriendSuggestionsService.Execute(Id, int.Parse(_topNumber));
+            }
+            catch { }
+        }
+    }
+}
